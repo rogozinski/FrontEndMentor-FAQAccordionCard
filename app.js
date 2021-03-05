1fabff39a32ec38245fd1b4cd4f57d2872c1a793
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-05 15:52:43 +0100

    project update
    recovering old project

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // const allHeaders = document.querySelectorAll(".cardHeader");
 // /* looping over h2 and removing class active */
 // function removeClass() {
@@ -14,3 +15,29 @@
 //         h2Header.classList.toggle("active");
 //     });
 // }
+=======
+const cardHeaders = document.querySelectorAll(".cardHeader");
+
+/* looping over h2 and removing class boldFont */
+function removeClass() {
+    for (let cardHeader of cardHeaders) {
+        cardHeader.style.pointerEvents = "auto";
+        const h2Header = cardHeader.firstElementChild;
+        h2Header.classList.remove("boldFont");
+
+    }
+}
+
+/* collapsed elements have class collapse. if clicked element is without that class we change font weight */
+for (let cardHeader of cardHeaders) {
+    cardHeader.addEventListener("click", function () {
+        removeClass();
+        cardHeader.style.pointerEvents = "none";
+        const h2Header = cardHeader.firstElementChild;
+        if (!cardHeader.classList.contains("collapse")) {
+            h2Header.classList.add("boldFont");
+        }
+
+    });
+}
+>>>>>>> parent of b0ff0b3 (project update)
