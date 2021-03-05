b8bbea35c1edf316aac7d4c91a40f2291001e779
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-05 15:33:37 +0100

    project update
    starting over..

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
+// const allHeaders = document.querySelectorAll(".cardHeader");
+// /* looping over h2 and removing class active */
+// function removeClass() {
+//     for (let cardHeader of allHeaders) {
+//         const h2Header = cardHeader.firstElementChild;
+//         h2Header.classList.remove("active");
+//     }
+// }
 
-const allHeaders = document.querySelectorAll(".cardHeader");
-/* looping over h2 and removing class active */
-function removeClass() {
-    for (let cardHeader of allHeaders) {
-        const h2Header = cardHeader.firstElementChild;
-        h2Header.classList.remove("active");
-    }
-}
-
-for (let cardHeader of allHeaders) {
-    cardHeader.addEventListener("click", function () {
-        const h2Header = cardHeader.firstElementChild;
-        removeClass();
-        h2Header.classList.toggle("active");
-    });
-}
+// for (let cardHeader of allHeaders) {
+//     cardHeader.addEventListener("click", function () {
+//         const h2Header = cardHeader.firstElementChild;
+//         removeClass();
+//         h2Header.classList.toggle("active");
+//     });
+// }
